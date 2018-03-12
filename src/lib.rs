2ef17d92ff06f67bf3985da15f0bdc2b2a3246e0
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2018-03-12 23:41:55 +0900

    Add vga_buffer.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,22 @@
+#![feature(const_fn)]
+#![feature(const_unique_new)]
 #![feature(lang_items)]
+#![feature(ptr_internals)]
+#![feature(unique)]
 #![no_std]
 
 extern crate rlibc;
+extern crate spin;
+extern crate volatile;
+
+#[macro_use]
+mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn rust_main() {
-    let hello = b"Hello World!";
-    let color_byte = 0x1f;
-
-    let mut hello_colored = [color_byte; 24];
-    for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i * 2] = *char_byte;
-    }
-
-    let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    unsafe { *buffer_ptr = hello_colored };
+    vga_buffer::clear_screen();
+    println!("Hello, World{}", "!");
+    println!("{}", { println!("inner"); "outer" });
 
     loop {}
 }
