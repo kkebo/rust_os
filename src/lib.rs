9f227acc70e6d560656b9e6a41b9fe069445d400
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2018-03-16 01:32:32 +0900

    Output message when panic occurs

--- src/lib.rs
+++ src/lib.rs
@@ -35,8 +35,11 @@ pub extern "C" fn rust_main(multiboot_information_address: usize) {
 #[lang = "eh_personality"]
 #[no_mangle]
 pub extern "C" fn eh_personality() {}
+
 #[lang = "panic_fmt"]
 #[no_mangle]
-pub extern "C" fn panic_fmt() -> ! {
+pub extern "C" fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str, line: u32) -> ! {
+    println!("\n\nPANIC in {} at line {}:", file, line);
+    println!("    {}", fmt);
     loop {}
 }
