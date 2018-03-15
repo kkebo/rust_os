24fcc9fb30adae74d1f304b0f463c787e4d6ab28
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2018-03-16 01:17:16 +0900

    Show multiboot information

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -5,6 +5,7 @@ section .text
 bits 32
 start:
     mov esp, stack_top
+    mov edi, ebx
 
     call check_multiboot
     call check_cpuid
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 #![feature(unique)]
 #![no_std]
 
+extern crate multiboot2;
 extern crate rlibc;
 extern crate spin;
 extern crate volatile;
@@ -13,12 +14,19 @@ extern crate volatile;
 mod vga_buffer;
 
 #[no_mangle]
-pub extern "C" fn rust_main() {
+pub extern "C" fn rust_main(multiboot_information_address: usize) {
     vga_buffer::clear_screen();
     println!("Hello, World{}", "!");
-    println!("{}", {
-        println!("inner");
-        "outer"
+
+    let boot_info = unsafe { multiboot2::load(multiboot_information_address) };
+    let memory_map_tag = boot_info.memory_map_tag().expect("Memory map tag required");
+
+    println!("memory areas:");
+    memory_map_tag.memory_areas().for_each(|area| {
+        println!(
+            "    start: 0x{:x}, size: 0x{:x}",
+            area.start_address(), area.size()
+        )
     });
 
     loop {}
