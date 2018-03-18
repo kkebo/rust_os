2a9bef8f980b1cb404c9ed25f3ef0a741cbfe940
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2018-03-19 01:44:56 +0900

    Use bootimage crate

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+#![feature(const_fn)]
+#![feature(const_unique_new)]
+#![feature(lang_items)]
+#![feature(ptr_internals)]
+#![feature(unique)]
+#![no_std]
+#![no_main]
+
+extern crate rlibc;
+extern crate spin;
+extern crate volatile;
+
+#[macro_use]
+mod vga_buffer;
+
+#[no_mangle]
+pub fn _start() -> ! {
+    vga_buffer::clear_screen();
+    println!("Hello, World{}", "!");
+
+    loop {}
+}
+
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern "C" fn eh_personality() {}
+
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern "C" fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str, line: u32) -> ! {
+    println!("\n\nPANIC in {} at line {}:", file, line);
+    println!("    {}", fmt);
+    loop {}
+}
