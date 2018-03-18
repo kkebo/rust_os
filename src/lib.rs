7f6a2b2e0c75ee57982bbed6b4158aaf13c62c7a
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2018-03-18 19:58:07 +0900

    Show more information

--- src/lib.rs
+++ src/lib.rs
@@ -33,19 +33,39 @@ pub extern "C" fn rust_main(multiboot_information_address: usize) {
             )
         });
 
-    println!("kernle sections:");
-    boot_info
+    let elf_sections_tag = boot_info
         .elf_sections_tag()
-        .expect("Elf-sections tag required")
+        .expect("Elf-sections tag required");
+    println!("kernle sections:");
+    elf_sections_tag.sections().for_each(|section| {
+        println!(
+            "    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
+            section.start_address(),
+            section.size(),
+            section.flags().bits()
+        )
+    });
+
+    let kernel_start = elf_sections_tag
         .sections()
-        .for_each(|section| {
-            println!(
-                "    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
-                section.start_address(),
-                section.size(),
-                section.flags().bits()
-            )
-        });
+        .map(|s| s.start_address())
+        .min()
+        .unwrap();
+    let kernel_end = elf_sections_tag
+        .sections()
+        .map(|s| s.end_address())
+        .max()
+        .unwrap();
+
+    println!(
+        "kernel_start: 0x{:x}, kernel_end: 0x{:x}",
+        kernel_start, kernel_end
+    );
+    println!(
+        "multiboot_start: 0x{:x}, multiboot_end: 0x{:x}",
+        boot_info.start_address(),
+        boot_info.end_address()
+    );
 
     loop {}
 }
